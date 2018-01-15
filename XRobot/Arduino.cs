c0d53a695418fe59563d1747ba9ffe9968e5a8ba
Ayman Mohammed <ayman-2015@outlook.com>
2018-01-15 20:37:47 +0800

    Add project files.

---
+++ XRobot/Arduino.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+namespace XRobot
+{
+    public class Arduino
+    {
+        private SerialDevice _serialDevice;
+
+        private DataReader _dataReader;
+        private DataWriter _dataWriter;
+
+        public bool IsOpen { get; set; }
+
+        public async Task<bool> Open(DeviceInformation device, uint baudRate = 9600,
+            SerialParity parity = SerialParity.None, ushort dataBits = 8,
+            SerialStopBitCount stopBits = SerialStopBitCount.One)
+        {
+            // Close open port
+            Close();
+            // Create a serial port device from the COM port device ID
+            _serialDevice = await SerialDevice.FromIdAsync(device.Id);
+            // If serial device is valid...
+            if (this._serialDevice != null)
+            {
+                _serialDevice.StopBits = stopBits;
+                _serialDevice.Parity = parity;
+                _serialDevice.BaudRate = baudRate;
+                _serialDevice.DataBits = dataBits;
+
+                // Create a single device writer for this port connection
+                _dataWriter = new DataWriter(this._serialDevice.OutputStream);
+
+                // Create a single device reader for this port connection
+                _dataReader = new DataReader(this._serialDevice.InputStream);
+
+                // Allow partial reads of the input stream
+                _dataReader.InputStreamOptions = InputStreamOptions.Partial;
+
+                // Port is now open
+                IsOpen = true;
+
+            }
+
+            return IsOpen;
+        }
+
+        public void Close()
+        {  
+            
+            // If serial device defined...
+            if (this._serialDevice != null)
+            {
+                // Dispose and clear device
+                this._serialDevice.Dispose();
+                this._serialDevice = null;
+            }
+
+            // If data reader defined...
+            if (this._dataReader != null)
+            {
+                // Detatch reader stream
+                this._dataReader.DetachStream();
+
+                // Dispose and clear data reader
+                this._dataReader.Dispose();
+                this._dataReader = null;
+            }
+
+            // If data writer defined...
+            if (this._dataWriter != null)
+            {
+                // Detatch writer stream
+                this._dataWriter.DetachStream();
+
+                // Dispose and clear data writer
+                this._dataWriter.Dispose();
+                this._dataWriter = null;
+            }
+            
+            // Port now closed
+            this.IsOpen = false;
+        }
+
+        public async Task WriteByteAsync(byte data)
+        {
+            // Write block of data to serial port
+            this._dataWriter.WriteByte(data);
+            // Transfer data to the serial device now
+            await this._dataWriter.StoreAsync();
+        }
+
+        public async Task<byte> ReadByteAsync()
+        {
+            await _dataReader.LoadAsync(1);
+            return _dataReader.ReadByte();
+        }
+
+        public async Task<DeviceInformationCollection> GetAvailableDevices()
+        {
+            var devices = await DeviceInformation.FindAllAsync(SerialDevice.GetDeviceSelector());
+
+            return devices;
+        }
+
+    }
+}
---
+++ XRobot/MainPage.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XRobot
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Arduino _arduino;
+        private DispatcherTimer _timer;
+        private byte _speed;
+        private byte _dirction;
+        private Color _color;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0,2);
+            _timer.Tick += _timer_Tick;
+           
+            _arduino = new Arduino();
+            _color = new Color();
+            _color.R = byte.MaxValue;
+            _color.B = 0;
+            _color.G = 0;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var devices = await _arduino.GetAvailableDevices();
+            portsList.ItemsSource = devices.Select(a => a.Name);
+            portsList.SelectedIndex = 0;
+
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_arduino.IsOpen)
+            {
+                var devices = await _arduino.GetAvailableDevices();
+                var device = devices.ToList().Find(d => d.Name == portsList.SelectedValue.ToString());
+
+                await _arduino.Open(device);
+                portsList.IsEnabled = false;
+                connectButton.Content = "Disconnect";
+                portsList.IsEnabled = false;
+                speedSlider.IsEnabled = true;
+                Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+                Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+                _timer.Start();
+            }
+            else
+            {
+                _arduino.Close();
+                portsList.IsEnabled = true;
+                connectButton.Content = "Connect";
+                speedSlider.IsEnabled = false;
+                _timer.Stop();
+                Window.Current.CoreWindow.KeyDown -= CoreWindow_KeyDown;
+                Window.Current.CoreWindow.KeyUp -= CoreWindow_KeyUp;
+            }
+        }
+
+
+        private async void _timer_Tick(object sender, object e)
+        {
+            if (_arduino.IsOpen)
+            {
+                await _arduino.WriteByteAsync(240);
+                var lightValue = await _arduino.ReadByteAsync();
+                _color.A = Convert.ToByte(lightValue + 50);
+                lightIntensityCircle.Fill = new SolidColorBrush(_color);
+                lightIntensity.Text = lightValue.ToString();
+            }
+        }
+
+        private async void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            _dirction = 0;
+            await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+        }
+
+        private async void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case Windows.System.VirtualKey.Left:
+                    _dirction = 128;
+                    await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+                    break;
+                case Windows.System.VirtualKey.Up:
+                    _dirction = 16;
+                    await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+                    break;
+                case Windows.System.VirtualKey.Right:
+                    _dirction = 64;
+                    await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+                    break;
+                case Windows.System.VirtualKey.Down:
+                    _dirction = 32;
+                    await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+                    break;
+                case Windows.System.VirtualKey.Space:
+                    _dirction = 0;
+                    await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+                    break;
+                case Windows.System.VirtualKey.Add:
+                    speedSlider.Value += 1;
+                    break;
+                case Windows.System.VirtualKey.Subtract:
+                    speedSlider.Value -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async void speedSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+            _speed = Convert.ToByte(e.NewValue);
+            await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+
+        }
+
+        private async void forwardBtn_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_arduino.IsOpen)
+            {
+                _dirction = 16;
+                await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+            }
+        }
+
+        private async void backwardBtn_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_arduino.IsOpen)
+            {
+                _dirction = 32;
+                await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+            }
+        }
+
+        private async void rightBtn_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_arduino.IsOpen)
+            {
+                _dirction = 64;
+                await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+            }
+        }
+
+        private async void leftBtn_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_arduino.IsOpen)
+            {
+                _dirction = 128;
+                await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+            }
+        }
+
+        private async void allBtn_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (_arduino.IsOpen)
+            {
+                _dirction = 0;
+                await _arduino.WriteByteAsync(Convert.ToByte(_speed + _dirction));
+            }
+        }
+
+    }
+}
